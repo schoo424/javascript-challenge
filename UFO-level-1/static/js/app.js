8e9ad2e2ccc1d4df49ba753d1e2bae24f297ec33
schoo424 <Sung.Choo@nike.com>
2021-01-09 13:41:22 -0800

    add my homework

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,75 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//Grab the data
+var tbody = d3.select("tbody");
+
+data.forEach(function(UFOData) {
+      console.log(UFOData);
+      var row = tbody.append("tr");
+
+    Object.entries(UFOData).forEach(function([key,value]) {
+        console.log(key,value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Event handler function for the form
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+// form.on("submit", runEnter);
+
+function runEnter() {
+// Prevent the page from refreshing
+    // d3.event.preventDefault();
+//clearing of the table each time
+
+tbody.html("")
+filteredData = tableData;
+
+// Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");    
+
+// Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);     
+
+    if (inputValue) {
+    //filtering of the data 
+        filteredData = filteredData.filter(row=> row.datetime == inputValue)
+    };
+
+//optional part --> adding 'Shape'
+    var shapeElement = d3.select("#shape");    
+
+    // Get the value property of the input element
+        var shapeValue = shapeElement.property("value");
+        console.log(shapeValue);   
+
+    //filtering of the data 
+    if(shapeValue){
+        filteredData = filteredData.filter(row=> row.shape == shapeValue)
+    };
+
+    filteredData.forEach(function(UFOData) {
+        console.log(UFOData);
+        var row = tbody.append("tr");
+  
+      Object.entries(UFOData).forEach(function([key,value]) {
+          console.log(key,value);
+          var cell = row.append("td");
+          cell.text(value);
+      });
+  });
+
+};
+
+
--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -2,31 +2,31 @@
 var tbody = d3.select("tbody");
 
 // Console.log the weather data from data.js
-// console.log(data);
+console.log(data);
 
 // // Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-// });
+data.forEach(function(weatherReport) {
+  console.log(weatherReport);
+});
 
 // // Step 2:  Use `Object.entries` to console.log each weather report value
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
+data.forEach(function(weatherReport) {
+  console.log(weatherReport);
 
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//   });
-// });
+  Object.entries(weatherReport).forEach(function([key, value]) {
+    console.log(key, value);
+  });
+});
 
 // // Step 3:  Use d3 to append one table row `tr` for each weather report object
 // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-// Object.entries(weatherReport).forEach(function([key, value]) {
-//   console.log(key, value);
-// });
-//   var row = tbody.append("tr");
-// });
+data.forEach(function(weatherReport) {
+  console.log(weatherReport);
+Object.entries(weatherReport).forEach(function([key, value]) {
+  console.log(key, value);
+});
+  var row = tbody.append("tr");
+});
 
 
 // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
@@ -44,32 +44,32 @@ data.forEach(function(weatherReport) {
 
 // // Step 5: Use d3 to update each cell's text with
 // // weather report values (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+data.forEach(function(weatherReport) {
+  console.log(weatherReport);
+  var row = tbody.append("tr");
+  Object.entries(weatherReport).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
 
 
-// // Bonus
-// data = data.filter((day) => {
-//   return day.high > 80;
-// })
+// Bonus
+data = data.filter((day) => {
+  return day.high > 80;
+})
 
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+data.forEach(function(weatherReport) {
+  console.log(weatherReport);
+  var row = tbody.append("tr");
+  Object.entries(weatherReport).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
